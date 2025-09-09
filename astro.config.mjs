eb04d79c7ad34cff4d2c3fe49482c5669ced0dbb
Leah Wasser <leah@pyopensci.org>
2025-09-08 19:51:27 -0600

    Change base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
 		site: 'https://allcontributors.org',
-		base: '/',
+		base: '/all-contributors/',
 		output: 'static',
 		redirects: {
 			// Later we can put redirects here if needed
