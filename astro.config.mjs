6817c3dff349bb8690e89ebac35908746b61ed8a
Jim Madge <jim@jmadge.com>
2026-03-03 10:40:08 +0000

    feat(i8n): move english docs to en locale (#1025)
    * feat(i8n): move english docs to en locale
    
    In preparation for i8n, the English docs are served at `/en` rather than
    the root. The redirect rule sending locales to the root of the site is
    also removed.
    
    fixes #1024
    
    * Correct links
    
    * Move index page out of locale
    
    * Update redirect rules

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,65 +13,71 @@ export default defineConfig({
     rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'append' }]],
   },
   output: 'static',
-
   redirects: {
-      // Later we can put redirects here if needed
+    // Later we can put redirects here if needed
   },
   integrations: [
-      starlight({
-          customCss: [
-              './src/styles/global.css',
-              './src/fonts/font-face.css',
-              ],
-          title: '',
-                  logo: {
-                      src: './src/assets/logo-full.svg',
-                  },
-          favicon: '/icons/favicon-16x16.png',
-          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/all-contributors/all-contributors' }],
-          sidebar: [
-              {
-                  label: 'Specification',
-                  items: [
-                      { label: 'spec overview', slug: 'overview' },
-                      { label: 'specification', slug: 'specification' },
-                      { label: 'emoji-key', slug: 'emoji-key' },
-                      { label: 'usage tips', slug: 'usage-tips' },
-                      { label: 'tooling', slug: 'tooling' }
-                  ],
-              },
-              {
-                  label: '@all-contributors Bot 🤖',
-                  items: [
-                      { label: 'bot overview', slug: 'bot/overview' },
-                      { label: 'installation', slug: 'bot/installation' },
-                      { label: 'usage', slug: 'bot/usage' },
-                      { label: 'configuration', slug: 'bot/configuration' },
-                      { label: 'faq', slug: 'bot/faq' }
-                  ],
-              },
-              {
-                  label: 'CLI',
-                  items: [
-                      { label: 'CLI overview', slug: 'cli/overview' },
-                      { label: 'installation', slug: 'cli/installation' },
-                      { label: 'usage', slug: 'cli/usage' },
-                      { label: 'configuration', slug: 'cli/configuration' }
-                  ],
-              },
-              {
-                  label: 'Project',
-                  items: [
-                      { label: 'contribute', slug: 'project/contribute' },
-                      { label: 'development', slug: 'project/development' },
-                      { label: 'maintain', slug: 'project/maintain' },
-                      { label: 'code-of-conduct', slug: 'project/code-of-conduct' },
-                      { label: 'implementations', slug: 'project/implementations' },
-                  ],
-              },
+    starlight({
+      customCss: [
+        './src/styles/global.css',
+        './src/fonts/font-face.css',
+      ],
+      title: '',
+      logo: {
+        src: './src/assets/logo-full.svg',
+      },
+      favicon: '/icons/favicon-16x16.png',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/all-contributors/all-contributors' }],
+      sidebar: [
+        {
+          label: 'Specification',
+          items: [
+            { label: 'spec overview', slug: 'overview' },
+            { label: 'specification', slug: 'specification' },
+            { label: 'emoji-key', slug: 'emoji-key' },
+            { label: 'usage tips', slug: 'usage-tips' },
+            { label: 'tooling', slug: 'tooling' }
           ],
-          }),
-      sitemap()
+        },
+        {
+          label: '@all-contributors Bot 🤖',
+          items: [
+            { label: 'bot overview', slug: 'bot/overview' },
+            { label: 'installation', slug: 'bot/installation' },
+            { label: 'usage', slug: 'bot/usage' },
+            { label: 'configuration', slug: 'bot/configuration' },
+            { label: 'faq', slug: 'bot/faq' }
+          ],
+        },
+        {
+          label: 'CLI',
+          items: [
+            { label: 'CLI overview', slug: 'cli/overview' },
+            { label: 'installation', slug: 'cli/installation' },
+            { label: 'usage', slug: 'cli/usage' },
+            { label: 'configuration', slug: 'cli/configuration' }
+          ],
+        },
+        {
+          label: 'Project',
+          items: [
+            { label: 'contribute', slug: 'project/contribute' },
+            { label: 'development', slug: 'project/development' },
+            { label: 'maintain', slug: 'project/maintain' },
+            { label: 'code-of-conduct', slug: 'project/code-of-conduct' },
+            { label: 'implementations', slug: 'project/implementations' },
+          ],
+        },
+      ],
+      // Set English default locale
+      defaultLocale: 'en',
+      locales: {
+        en: {
+          label: 'English',
+        },
+      },
+    }),
+    sitemap()
   ],
   vite: {
     plugins: [tailwindcss()],
